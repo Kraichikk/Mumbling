93ac12c8cdd474d068e3035ee3cf67c23cdc9d1f
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 13:24:55 +0200

    Add project files.

---
+++ Mumbling/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Mumbling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Accum("abcd"));
+        }
+        public static String Accum(string b)
+        {
+            string s = b.ToLower();
+
+            string val = "";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = -1; j < i; j++)
+                {
+                    if (i == 0)
+                    {
+                        val += s[i].ToString().ToUpper();
+                    }
+                    if (i > 0)
+                    {
+                        if (j == -1)
+                        {
+                            val += s[i].ToString().ToUpper();
+                        }
+                        else
+                        {
+                            val += s[i];
+                        }
+                    }
+                }
+                if (i != s.Length - 1)
+                    val += "-";
+            }
+            return val;
+        }
+    }
+}
